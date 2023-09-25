d37275960fddf1e3912f555c7680e9e4d879f498
F-Rivers <fxjxrx@gmail.com>
2023-09-24 22:58:35 -0700

    test

---
+++ src/Realtor.js
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Realtor Website</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <h1>Realtor Website</h1>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">About Us</a>
+      <a href="#">Services</a>
+      <a href="#">Testimonials</a>
+      <a href="#">Contact Us</a>
+    </nav>
+  </header>
+  <main>
+    <section id="home">
+      <h2>Welcome to our website!</h2>
+      <p>We are a team of experienced realtors who are dedicated to helping you find the perfect home. We offer a wide range of services, including buying, selling, and property management.</p>
+      <a href="#">Learn more</a>
+    </section>
+    <section id="about-us">
+      <h2>About Us</h2>
+      <p>We are a family-owned and operated business that has been serving the community for over 20 years. We are committed to providing our clients with the best possible service and helping them achieve their real estate goals.</p>
+      <p>Our team of experienced realtors has a deep understanding of the local market and can help you find the perfect home for your needs. We also offer a variety of services to make the buying or selling process as smooth as possible.</p>
+      <a href="#">Learn more</a>
+    </section>
+    <section id="services">
+      <h2>Services</h2>
+      <ul>
+        <li>Buying</li>
+        <li>Selling</li>
+        <li>Property management</li>
+        <li>Relocation assistance</li>
+      </ul>
+      <a href="#">Learn more</a>
+    </section>
+    <section id="testimonials">
+      <h2>Testimonials</h2>
+      <p>"We had a great experience working with the team at Realtor Website. They were very knowledgeable and helpful, and they made the whole process of buying a home a lot easier." - John Smith</p>
+      <p>"I would highly recommend Realtor Website to anyone looking to buy or sell a home. They are a great team of professionals who will go above and beyond to help you achieve your goals." - Jane Doe</p>
+      <a href="#">Read more testimonials</a>
+    </section>
+    <section id="contact-us">
+      <h2>Contact Us</h2>
+      <p>We would love to hear from you! Please fill out the form below to get in touch with us.</p>
+      <form action="#">
+        <input type="text" name="name" placeholder="Your name">
+        <input type="email" name="email" placeholder="Your email">
+        <input type="text" name="message" placeholder="Your message">
+        <input type="submit" value="Submit">
+      </form>
+    </section>
+  </main>
+  <footer>
+    <p>Copyright &copy; 2023 Realtor Website</p>
+  </footer>
+</body>
+</html>
